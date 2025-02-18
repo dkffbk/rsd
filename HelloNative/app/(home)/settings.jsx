a245d9bff0733e2156d2fcc44379175e8f3ee672
Bread <dkffbk@gmail.com>
2025-02-18 11:11:54 +0630

    Chapter-13 HelloNative

---
+++ HelloNative/app/(home)/settings.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function settings() {
+  return (
+    <View
+      style={{
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontWeight: "bold" }}>Settings</Text>
+    </View>
+  );
+}
